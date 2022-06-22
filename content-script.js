9ba8cba1d80e06bfa532c4ba8adab7aaefd87b87
Annalise Mariottini <annalise.mariottini@columbia.edu>
2022-06-22 11:51:53 -0400

    use img for svg

--- content-script.js
+++ content-script.js
@@ -1,26 +1,22 @@
+const COPY = {
+    noResultsText: 'Couldn\'t find anything for that, sorry...',
+    favoriteButtonText: 'Add product to Saved Products',
+    closeButtonText: 'Close window',
+};
+
 const popupContainer = document.createElement('div');
 const popupDragger = document.createElement('div');
-const popupCloseButton = document.createElement('div');
-
-const closeButtonSVG = `
-<svg viewPort="0 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg">
-  <line x1="1" y1="19" x2="19" y2="1" stroke="black" stroke-width="2"/>
-  <line x1="1" y1="1" x2="19" y2="19" stroke="black" stroke-width="2"/>
-</svg>
-`;
-const heartSVG = `
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" height="39" width="39" viewBox="-4 1 40 40">
-  <path d="M 0 20 v -20 h 20 a 10 10 90 0 1 0 20 a 10 10 90 0 1 -20 0 z" stroke="rgb(17,17,17)" stroke-width="3" fill="none" transform="rotate(225,16,16)"></path>
-</svg>
-`;
+const popupCloseButton = document.createElement('img');
 
 popupContainer.className = 'ewg-popup-container';
 popupDragger.className = 'ewg-popup-dragger';
 popupCloseButton.className = 'ewg-popup-close-button';
 
 popupContainer.style.top = `${window.innerHeight}px`;
+popupCloseButton.src = chrome.runtime.getURL('/images/closeButton.svg');
+popupCloseButton.alt = COPY.closeButtonText;
+popupCloseButton.title = COPY.closeButtonText;
 popupCloseButton.style.display = 'none';
-popupCloseButton.innerHTML = closeButtonSVG;
 
 popupContainer.appendChild(popupDragger);
 popupContainer.appendChild(popupCloseButton);
@@ -72,12 +68,15 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const popupContent = document.createElement('div');
     popupContent.className = 'ewg-popup-content';
 
-    const favoriteElement = document.createElement('div');
+
+    const favoriteElement = document.createElement('img');
+    favoriteElement.src = chrome.runtime.getURL('/images/heart_empty.svg');
+    favoriteElement.alt = COPY.favoriteButtonText;
+    favoriteElement.title = COPY.favoriteButtonText; 
     favoriteElement.className = 'favorite-product';
-    favoriteElement.innerHTML = heartSVG;
 
     const addFavorite = (id, element) => {
-        element.getElementsByTagName('path')[0].style.fill = 'red';
+        element.getElementsByClassName('favorite-product')[0].src = chrome.runtime.getURL('/images/heart_red.svg');
         chrome.storage.sync.set({ [id]: element.outerHTML }, () => {});
     };
 
@@ -95,7 +94,10 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
 
     if (products.length === 0) {
-        popupContent.innerHTML += '<div class="no-results">Couldn\'t find anything for that, sorry...</div>';
+        const noResultsElement = document.createElement('div');
+        noResultsElement.class = 'no-results';
+        noResultsElement.textContent = COPY.noResultsText;
+        popupContent.appendChild(noResultsElement);
     }
 
     const popupContentElement = document.getElementsByClassName('ewg-popup-content');
